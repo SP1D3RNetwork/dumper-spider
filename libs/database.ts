f480d52e72db804f7f318a47573bdfbafc152afa
SP1D3R <99975544+SP1D3RNetwork@users.noreply.github.com>
2022-03-27 21:03:09 +0200

    Update database.ts

--- libs/database.ts
+++ libs/database.ts
@@ -1,11 +1,11 @@
 import mysql from "serverless-mysql";
 const db = mysql({
   config: {
-    host: "137.74.202.159",
-    port: 3306,
-    database: "s25956_data_testing",
-    user: "u25956_kmGTt0hViR",
-    password: "1o64uKef27TVaJBSwmAF5Wy6",
+    host: "IP",
+    port: PORT,
+    database: "DATABASE-NAME",
+    user: "USER",
+    password: "PASSWORD",
   },
 });
 export default async function excuteQuery({ query, values }: any) {
